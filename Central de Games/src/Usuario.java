4be2a3032d87130524b32011319d8f44aba30c92
Andrewscc <andrews.araujo@ccc.ufcg.edu.br>
2017-02-07 09:58:51 -0300

    fim de aula

--- Central de Games/src/Usuario.java
+++ Central de Games/src/Usuario.java
@@ -2,8 +2,38 @@ import java.util.ArrayList;
 
 public class Usuario {
 
-	private String nome;
-	private ArrayList jogos;
-	private double dinheiro;
+	protected String nome;
+	protected ArrayList jogosComprados;
+	protected double dinheiro;
+
+	public Usuario(String nome, double dinheiro) {
+		this.nome = nome;
+		this.dinheiro = dinheiro;
+		this.jogosComprados = new ArrayList<Jogo>();
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public ArrayList getJogos() {
+		return jogos;
+	}
+
+	public void setJogos(ArrayList jogos) {
+		this.jogos = jogos;
+	}
+
+	public double getDinheiro() {
+		return dinheiro;
+	}
+
+	public void setDinheiro(double dinheiro) {
+		this.dinheiro = dinheiro;
+	}
 
 }
